1ac2beadcc9680d70958e5fe411435ce7090c05c
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-04 23:03:32 -0600

    added style.css file

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!--Custom CSS link-->
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!--Jquery required for bootstrap components-->
+		<script src="https://cdnjs.cloudflare.com/ajax/Libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+		<title>Bootstrap Challenge</title>
+	</head>
+
+	<body>
+		<header>
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+								  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Raúl Villarreal</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">Portfolio</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</header>
+			<div class="panel panel-default">
+				<div class="panel-body">
+				Welcome to my Website
+				</div>
+			</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="picture">
+						<img src="images/myprofile.jpg" class="img-responsive" alt="Responsive image">
+					</div>
+				</div>
+				<div class="col-md-9">
+					<div class="content">
+						<p>Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu
+						Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung
+						Fu Ipsum</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-3">
+					<img src="images/myprofile.jpg" alt="my profile" class="img-responsive">
+					<p>Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu
+						Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung
+						Fu Ipsum</p>
+				</div>
+				<div class="col-md-3">
+					<img src="images/myprofile.jpg" alt="my profile" class="img-responsive">
+					<p>Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu
+						Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung
+						Fu Ipsum</p>
+				</div>
+				<div class="col-md-3">
+					<img src="images/myprofile.jpg" alt="my profile" class="img-responsive">
+					<p>Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu
+						Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung
+						Fu Ipsum</p>
+				</div>
+				<div class="col-md-3">
+					<img src="images/myprofile.jpg" alt="my profile" class="img-responsive">
+					<p>Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu
+						Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung Fu Ipsum Loren Ipsum Kung
+						Fu Ipsum</p>
+				</div>
+
+
+			</div>
+
+
+
+
+		</div>
+	</body>
+	<footer class="sfooter">
+		<div class="sfooter-content">
+			<p>Copyright</p>
+		</div>
+	</footer>
+</html>
